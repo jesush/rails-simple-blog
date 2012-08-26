74cd3cb70052755f41c29c9cda84224f8d92630f
Jesus H <jesus@codeawesome.com>
2012-08-26 02:47:03 -0500

    Fix paths to application

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/deployer/apps/blog/current"
+root = "/home/deployer/apps/rails-simple-blog/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
